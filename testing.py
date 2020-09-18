10cbd1b173eb9d53869a03199daf9e9e8c73cd2a
bluedolphin264 <50972662+bluedolphin264@users.noreply.github.com>
2020-09-18 13:07:47 +1000

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def send_to_ifttt(event):
-    url = 'https://maker.ifttt.com/trigger/' + event + '/with/key/jfRaKs196E7wf4oJWHIa2GA1RoEG4HwBTQJ6b19tvVl'
-    requests.get(url)
-
-
-send_to_ifttt('switched_off')
