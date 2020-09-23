86bdcae992042ed08e88e4dce4882b0d50429076
pheaton264 <paddyheaton264@gmail.com>
2020-09-23 20:46:32 +1000

    test

--- news.py
+++ news.py
@@ -4,12 +4,12 @@ API_KEY = 'c10aaad5b172490b9ff1b954d6cec496'
 
 
 def get_top_news():
-    top_url = 'http://newsapi.org/v2/top-headlines?country=au&apiKey=' + API_KEY
+    top_url = 'http://newsapi.org/v2/top-headlines?country=au&from=2020-09-23&apiKey=' + API_KEY
     return get_news(top_url)
 
 
 def get_news_about(topic):
-    question_url = 'http://newsapi.org/v2/everything?q=' + topic + '&apiKey=' + API_KEY
+    question_url = 'http://newsapi.org/v2/everything?q=' + topic + '&sortBy=popularity&from=2020-09-23&apiKey=' + API_KEY
     return get_news(question_url)
 
 
@@ -19,7 +19,7 @@ def get_news(url):
     if info['totalResults'] == 0:
         return "Couldn't find any articles about that :("
     message = ""
-    for i in range(3):
+    for i in range(10 if info['totalResults'] >= 10 else info['totalResults']):
         top_article = info['articles'][i]
         message += top_article['title'] + '\n' + top_article['url'] + '\n\n'
     return message
